b4086270b6d35dda034ed67359ade74d014391f5
Anant Desai <anant.desai@baseten.co>
2026-04-12 14:53:20 -0700

    added toy numpy logic

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import numpy as np
+
+
 def main():
-    print("Hello from neural-net!")
+    # Create a simple 2D array
+    data = np.array([[1, 2], [3, 4]])
+
+    print("Hello from uv!")
+    print(f"Here is a NumPy matrix:\n{data}")
+    print(f"The mean is: {np.mean(data)}")
 
 
 if __name__ == "__main__":
