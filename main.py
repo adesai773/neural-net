a0b01f5e296c25bbe7a1cae1dc179712188d4a64
Anant Desai <anant.desai@baseten.co>
2026-04-16 12:10:56 -0700

    added example for summing over axis

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def main():
     # Create a simple 2D array
-    data = np.array([[1, 2], [3, 4]])
+    data = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 
     print("Hello from uv!")
     print(f"Here is a NumPy matrix:\n{data}")
-    print(f"The mean is: {np.mean(data)}")
+    print(f"The sum is: {np.sum(data, axis=1)}")
 
 
 if __name__ == "__main__":
